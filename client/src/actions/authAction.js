78728744ce017c2d94d56737b77419dd9f2582d3
Mark Gilmore <mrkgilmore@gmail.com>
2019-03-07 01:52:50 +0000

    registration and form error handling working

--- client/src/actions/authAction.js
+++ client/src/actions/authAction.js
@@ -1,10 +1,17 @@
-import { TEST_DISPATCH } from './types';
+//import { TEST_DISPATCH } from './types';
+import axios from 'axios';
+import { GET_ERRORS } from './types';
 
 // User Registration
-export const registerUser = (userData) => {
-  return {
-    type: TEST_DISPATCH,
-    payload: userData
-  };
-     
+export const registerUser = (userData, history) => dispatch => {
+  axios
+    .post('/api/users/register', userData)
+    .then(res => history.push('/login'))
+    //.then(res => console.log(res.data))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
 };
--- client/src/components/authentication/Register.js
+++ client/src/components/authentication/Register.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { registerUser } from '../../actions/authAction';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 
 // registration construcor
 class Register extends Component {
@@ -18,6 +21,13 @@ class Register extends Component {
         this.onSubmit = this.onSubmit.bind(this);
     }
 
+    // runs when component receives new properties
+    componentWillReceiveProps (nextProps) {
+      if (nextProps.errors) {
+        this.setState({ errors: nextProps.errors });
+      }
+    }
+
     onChange(e) {
         this.setState({[e.target.name]: e.target.value});
     }
@@ -32,11 +42,15 @@ class Register extends Component {
             password2: this.state.password2
         }
 
-        this.props.registerUser(newUser);
+        this.props.registerUser(newUser, this.props.history);
         //console.log(newUser);
     }
 
   render() {
+
+    // pulls errors from state
+    const { errors } = this.state;
+
     return (
         <div className="register">
           <div className="container">
@@ -44,18 +58,22 @@ class Register extends Component {
               <div className="col-md-8 m-auto">
                 <h1 className="display-4 text-center">Register</h1>
                 <p className="lead text-center">Create your Book-King account</p>
-                <form onSubmit={ this.onSubmit }>
+                <form noValidate onSubmit={ this.onSubmit }>
                   <div className="form-group">
-                    <input type="text" className="form-control form-control-lg" placeholder="Name" name="name" value = { this.state.name } onChange = { this.onChange } />
+                    <input type="text" className={ classnames("form-control form-control-lg", { "is-invalid": errors.name }) } placeholder="Name" name="name" value = { this.state.name } onChange = { this.onChange } />
+                    { errors.name && (<div className="invalid-feedback">{ errors.name }</div>)}
                   </div>
                   <div className="form-group">
-                    <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" value = { this.state.email } onChange = { this.onChange } />
+                    <input type="email" className={ classnames("form-control form-control-lg", { "is-invalid": errors.email }) } placeholder="Email Address" name="email" value = { this.state.email } onChange = { this.onChange } />
+                    { errors.email && (<div className="invalid-feedback">{ errors.name }</div>)}
                   </div>
                   <div className="form-group">
-                    <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" value = { this.state.password } onChange = { this.onChange } />
+                    <input type="password" className={ classnames("form-control form-control-lg", { "is-invalid": errors.password }) } placeholder="Password" name="password" value = { this.state.password } onChange = { this.onChange } />
+                    { errors.password && (<div className="invalid-feedback">{ errors.name }</div>)}
                   </div>
                   <div className="form-group">
-                    <input type="password" className="form-control form-control-lg" placeholder="Confirm Password" name="password2" value = { this.state.password2 } onChange = { this.onChange } />
+                    <input type="password" className={ classnames("form-control form-control-lg", { "is-invalid": errors.password2 }) } placeholder="Confirm Password" name="password2" value = { this.state.password2 } onChange = { this.onChange } />
+                    { errors.password2 && (<div className="invalid-feedback">{ errors.name }</div>)}
                   </div>
                   <input type="submit" className="btn btn-success btn-block mt-4" />
                 </form>
@@ -67,4 +85,16 @@ class Register extends Component {
   }
 }
 
-export default connect(null, { registerUser })(Register);
+Register.propTypes = {
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+// mapped state to prop and set auth to prop
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(mapStateToProps, { registerUser })(withRouter(Register));
