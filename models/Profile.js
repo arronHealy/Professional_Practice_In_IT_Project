df9076f2c0ab04083fd5a4248fc573e6cf4d9499
arronHealy <G00346883@gmit.ie>
2019-03-09 16:02:24 +0000

    Profile model set up, protected profile route added

--- models/Profile.js
+++ models/Profile.js
@@ -4,6 +4,71 @@ const Schema = mongoose.Schema;
 
 const ProfileSchema = new Schema({
   //build profile for user
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  profileImage: {
+    type: String,
+    default: "../Images/noProfilePic.jpg"
+  },
+  location: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  favoriteGenres: {
+    type: [String],
+    required: true
+  },
+  bio: {
+    type: String
+  },
+  books: [
+    {
+      title: {
+        type: String,
+        required: true
+      },
+      author: {
+        type: String,
+        required: true
+      },
+      price: {
+        type: Number,
+        required: true
+      },
+      publishedDate: {
+        type: Date
+      },
+      description: {
+        type: String,
+        required: true
+      },
+      bookImage: {
+        type: String
+      }
+    }
+  ],
+  socialLinks: {
+    youtube: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    }
+  }
 });
 
 module.exports = Profile = mongoose.model("profile", ProfileSchema);
--- routes/usersProfile.js
+++ routes/usersProfile.js
@@ -6,7 +6,32 @@ const mongoose = require("mongoose");
 
 const passport = require("passport");
 
+//get profile and user models
+const User = require("../models/User");
+
+const Profile = require("../models/Profile");
+
 //initial test route
 router.get("/test", (req, res) => res.json({ msg: "profile route working" }));
+
+//get current user passport protected route
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.notfound = "There is no profile for this user";
+          return res.status(404).json(errors);
+        }
+
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
 
 module.exports = router;
