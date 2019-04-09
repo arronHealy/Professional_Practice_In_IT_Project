e04aa8ba39b2ce4afbe6dde810e69eb7651ada2c
arronHealy <G00346883@gmit.ie>
2019-04-10 00:33:21 +0100

    updated text style for book-item component

--- client/src/components/profile/ProfileBookItem.js
+++ client/src/components/profile/ProfileBookItem.js
@@ -22,12 +22,12 @@ class ProfileBookItem extends Component {
             />
           </div>
           <div className="col-lg-6 col-md-4 col-8">
-            <p>Title: {book.title}</p>
-            <p>Author: {book.author}</p>
-            <p>Genre: {book.genre}</p>
-            <p>Condition: {book.condition}</p>
-            <p>Description: {book.description}</p>
-            <p>Price: &euro;{book.price}</p>
+            <p className="lead">Title: {book.title}</p>
+            <p className="lead">Author: {book.author}</p>
+            <p className="lead">Genre: {book.genre}</p>
+            <p className="lead">Condition: {book.condition}</p>
+            <p className="lead">Description: {book.description}</p>
+            <p className="lead">Price: &euro;{book.price}</p>
             <button type="button" className="btn btn-success">
               Add Book To Your Cart
             </button>
