0eea4aa92d736f4953ea804e6c74cefb99b618b3
arronHealy <G00346883@gmit.ie>
2019-04-27 15:01:22 +0100

    push build product

---
+++ client/build/precache-manifest.3fcfaf9236c67a2547cb9a0f44f60139.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0d26039b9a011584d595b77a6879368",
+    "url": "/static/media/spinner.f0d26039.gif"
+  },
+  {
+    "revision": "625d1aff2d58f024870ef61b95680166",
+    "url": "/static/media/noBookImage.625d1aff.jpg"
+  },
+  {
+    "revision": "58563c89a8e464fad5ff277b6490751c",
+    "url": "/static/media/books3.58563c89.jpg"
+  },
+  {
+    "revision": "96d19931ff3a0447ee442f917a67307b",
+    "url": "/static/media/books2.96d19931.jpg"
+  },
+  {
+    "revision": "eaa248095b876554de9a60e52babe766",
+    "url": "/static/media/books.eaa24809.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a26ee8723d2bc9c4da65",
+    "url": "/static/js/main.a26ee872.chunk.js"
+  },
+  {
+    "revision": "34eef6018895938266f4",
+    "url": "/static/js/2.34eef601.chunk.js"
+  },
+  {
+    "revision": "a26ee8723d2bc9c4da65",
+    "url": "/static/css/main.3faa920c.chunk.css"
+  },
+  {
+    "revision": "eb18111322de3f3cea6c39e6e8c052c4",
+    "url": "/index.html"
+  }
+];
